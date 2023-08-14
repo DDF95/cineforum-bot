7496bf88ce778aee97249010830f472a7dcac7d8
DDF95 <difebbo.davide@gmail.com>
2023-08-14 18:03:58 +0200

    add group-specific movie lists

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import sys
 from pathlib import Path
 
-from telegram import Update
+from telegram import Update, constants
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
@@ -21,32 +21,6 @@ def is_admin(user_id: int):
         return False
 
 
-async def random_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if is_admin(update.message.from_user.id):
-        movie_data = load_movie_data()
-
-        if not movie_data:
-            await update.message.reply_text("La lista è vuota. Usa il comando /aggiungi per aggiungere dei film.")
-            return
-
-        random_user_id = random.choice(list(movie_data.keys()))
-        user_data = movie_data[random_user_id]
-        user_name = user_data["first_name"]
-        user_movies = user_data["movies"]
-
-        if not user_movies:
-            await update.message.reply_text(f"Nessun film aggiunto da {user_name}.")
-            return
-
-        random_movie = random.choice(user_movies)
-        user_movies.remove(random_movie)
-        if len(user_movies) == 0:
-            del movie_data[random_user_id]
-        
-        save_movie_data(movie_data)
-        await update.message.reply_html(f"Film scelto casualmente: <i>{random_movie}</i>\n\n(aggiunto da <b>{user_name}</b>)")
-
-
 def load_movie_data():
     if json_file_path.exists():
         with open(json_file_path, "r") as file:
@@ -59,14 +33,60 @@ def save_movie_data(data):
         json.dump(data, file, indent=4)
 
 
+async def random_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.chat.type == constants.ChatType.PRIVATE:
+        await update.message.reply_text("Questo comando è disponibile solo nei gruppi.")
+        return
+    
+    if not update.message.from_user.id in [admin.user.id for admin in await context.bot.get_chat_administrators(update.message.chat.id)]:
+        await update.message.reply_text("Sorry solo lx admin possono usare questo comando, serve ad evitare che il comando venga usato per sbaglio")
+        return
+    
+    movie_data = load_movie_data()
+
+    group_id = str(update.message.chat.id)
+    if group_id not in movie_data:
+        await update.message.reply_text("La lista del gruppo è vuota. Usa il comando /aggiungi per aggiungere dei film.")
+        return
+
+    group_movies = movie_data[group_id]
+
+    all_users = list(group_movies.keys())
+    if not all_users:
+        await update.message.reply_text("La lista del gruppo è vuota. Usa il comando /aggiungi per aggiungere dei film.")
+        return
+
+    random_user_id = random.choice(all_users)
+    user_data = group_movies[random_user_id]
+    user_name = user_data["first_name"]
+    user_movies = user_data["movies"]
+
+    if not user_movies:
+        await update.message.reply_text(f"Nessun film aggiunto da {user_name}.")
+        return
+
+    random_movie = random.choice(user_movies)
+    user_movies.remove(random_movie)
+    if len(user_movies) == 0:
+        del group_movies[random_user_id]
+
+    save_movie_data(movie_data)
+    await update.message.reply_html(f"Film scelto casualmente: <i>{random_movie}</i>\n\n(aggiunto da <b>{user_name}</b>)")
+
+
 async def add_movies(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.chat.type == constants.ChatType.PRIVATE:
+        await update.message.reply_text("Questo comando è disponibile solo nei gruppi.")
+        return
+    
     if not context.args:
         await update.message.reply_html("Inserisci almeno un film. Se vuoi puoi inserirne più di uno andando a capo. Esempio:\n\n<code>/aggiungi Film 1\nFilm 2\nFilm 3</code>")
         return
 
-    user_id = update.message.from_user.id
+    group_id = str(update.message.chat.id)
+    user_id = str(update.message.from_user.id)
     first_name = update.message.from_user.first_name
-    
+
     movies = update.message.text
     movies = movies.replace("/aggiungi \n", "")
     movies = movies.replace("/aggiungi" , "")
@@ -75,30 +95,43 @@ async def add_movies(update: Update, context: ContextTypes.DEFAULT_TYPE):
     movies = [movie.lstrip("- ") for movie in movies if movie.strip()]
 
     movie_data = load_movie_data()
-    if str(user_id) not in movie_data:
-        movie_data[str(user_id)] = {"first_name": first_name, "movies": []}
-    movie_data[str(user_id)]["movies"].extend(movies)
+    if group_id not in movie_data:
+        movie_data[group_id] = {}
+    if user_id not in movie_data[group_id]:
+        movie_data[group_id][user_id] = {"first_name": first_name, "movies": []}
+    movie_data[group_id][user_id]["movies"].extend(movies)
 
     save_movie_data(movie_data)
     await update.message.reply_text("Fatto!")
 
 
 async def generate_movie_list(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.chat.type == constants.ChatType.PRIVATE:
+        await update.message.reply_text("Questo comando è disponibile solo nei gruppi.")
+        return
+    
     movie_data = load_movie_data()
 
-    if not movie_data:
-        await update.message.reply_text("La lista è vuota. Usa il comando /aggiungi per aggiungere dei film.")
+    group_id = str(update.message.chat.id)
+    if group_id not in movie_data:
+        await update.message.reply_text("La lista del gruppo è vuota. Usa il comando /aggiungi per aggiungere dei film.")
+        return
+
+    group_movies = movie_data[group_id]
+
+    if not group_movies:
+        await update.message.reply_text("La lista del gruppo è vuota. Usa il comando /aggiungi per aggiungere dei film.")
         return
 
     movie_list = []
-    for user_id, user_data in movie_data.items():
+    for user_id, user_data in group_movies.items():
         first_name = user_data["first_name"]
         movies = user_data["movies"]
         movie_list.append((first_name, movies))
 
     movie_list.sort(key=lambda x: x[0])
     message = "<b>Lista dei film da vedere</b>\n"
-    
+
     movie_counter = 1
     for first_name, movies in movie_list:
         message += f"\nScelti da <b>{first_name}</b>:\n"
@@ -106,16 +139,27 @@ async def generate_movie_list(update: Update, context: ContextTypes.DEFAULT_TYPE
             message += f"{movie_counter}. {movie}\n"
             movie_counter += 1
     message += f"\n<i>Per aggiungere un film alla lista usa il comando /aggiungi</i>"
-            
+
     await update.message.reply_html(message)
 
 
 async def delete_movies(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.chat.type == constants.ChatType.PRIVATE:
+        await update.message.reply_text("Questo comando è disponibile solo nei gruppi.")
+        return
+    
     movie_data = load_movie_data()
 
+    group_id = str(update.message.chat.id)
     user_id = str(update.message.from_user.id)
-    if user_id in movie_data:
-        deleted_movies = movie_data.pop(user_id)["movies"]
+
+    if group_id in movie_data and user_id in movie_data[group_id]:
+        deleted_movies = movie_data[group_id][user_id]["movies"]
+        del movie_data[group_id][user_id]
+
+        if not movie_data[group_id]:
+            del movie_data[group_id]
+
         save_movie_data(movie_data)
 
         message = "Film eliminati:\n"
@@ -123,7 +167,7 @@ async def delete_movies(update: Update, context: ContextTypes.DEFAULT_TYPE):
             message += f"{idx}. {movie}\n"
         await update.message.reply_text(message)
     else:
-        await update.message.reply_text("Non hai film da eliminare")
+        await update.message.reply_text("Non hai film da eliminare.")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
